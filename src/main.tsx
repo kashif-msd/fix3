dcb964264b757a5399f8a30792274b73b96fe2b7
kashif-msd <2019-cs-4080@uosahiwal.edu.pk>
2024-05-27 05:37:39 -0700

    m

---
+++ src/main.tsx
@@ -0,0 +1,81 @@
+import { createTheme, ThemeProvider } from "@material-ui/core";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import {
+  AlphaWalletAdapter,
+  CloverWalletAdapter,
+  LedgerWalletAdapter,
+  PhantomWalletAdapter,
+  SafePalWalletAdapter,
+  SolflareWalletAdapter,
+  SolongWalletAdapter,
+} from "@solana/wallet-adapter-wallets";
+
+import { useMemo } from "react";
+
+import Home from "./Home";
+import { rpcHost, candyMachineId, network } from "./config";
+
+const theme = createTheme({
+  palette: {
+    type: "dark",
+  },
+  overrides: {
+    MuiButtonBase: {
+      root: {
+        justifyContent: "flex-start",
+      },
+    },
+    MuiButton: {
+      root: {
+        textTransform: undefined,
+        padding: "12px 16px",
+      },
+      startIcon: {
+        marginRight: 8,
+      },
+      endIcon: {
+        marginLeft: 8,
+      },
+    },
+  },
+});
+
+const Main = ({}) => {
+  // Custom RPC endpoint.
+  const endpoint = useMemo(() => rpcHost, []);
+
+  // @solana/wallet-adapter-wallets includes all the adapters but supports tree shaking and lazy loading --
+  // Only the wallets you configure here will be compiled into your application, and only the dependencies
+  // of wallets that your users connect to will be loaded.
+  const wallets = useMemo(
+    () => [
+      new AlphaWalletAdapter(),
+      new CloverWalletAdapter(),
+      new LedgerWalletAdapter(),
+      new PhantomWalletAdapter(),
+      new SafePalWalletAdapter(),
+      new SolflareWalletAdapter({ network }),
+      new SolongWalletAdapter(),
+      new SolflareWalletAdapter(),
+    ],
+    []
+  );
+
+  return (
+    <ThemeProvider theme={theme}>
+      <ConnectionProvider endpoint={endpoint}>
+        <WalletProvider wallets={wallets} autoConnect={true}>
+          <WalletModalProvider>
+            <Home candyMachineId={candyMachineId} />
+          </WalletModalProvider>
+        </WalletProvider>
+      </ConnectionProvider>
+    </ThemeProvider>
+  );
+};
+
+export default Main;
